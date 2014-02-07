9eae15574bc30a36134eac65cec986b0a3c5e921
Darragh ORiordan <darragh.oriordan@gmail.com>
2014-02-08 00:15:37 +1300

    added bootstrap
    decent start proj for future bootstrapped project

---
+++ WhosGiggin/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WhosGiggin
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
